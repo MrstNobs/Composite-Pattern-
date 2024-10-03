7194b12850fe10b638269a1aa05639f6bcf466af
Kauã Daniel Cardoso de Brito <kauandaniel2005@gmail.com>
2024-10-03 15:44:57 -0300

    asdas
    asd

--- Update.php
+++ Update.php
@@ -1,37 +1,37 @@
 <?php 
 
-/* função autoload 
-* Carrega uma classe quando ela é necessária, ou seja, quando é instanciado 
-* pela primeira vez
-*/
+    /* função autoload 
+    * Carrega uma classe quando ela é necessária, ou seja, quando é instanciado 
+    * pela primeira vez 
+    */
 
-spl_autoload_register(function ($classe) {
-    if (file_exists("{$classe}.class.php")) {
+    spl_autoload_register(function ($classe) {
+        if (file_exists("{$classe}.class.php")) {
         include_once "{$classe}.class.php";
     }
-});
+    });
 
-// cria um criterio de seleção de dados
+    // cria um criterio de seleção de dados
+    $criteria = new TCriteria;
+    $criteria->add(new TFilter('id','=','3'));
 
-$criteria = new TCriteria;
-$criteria->add(new TFilter('id','=','3'));
+    //cria instrução de UPDATE
+    $sql = new TSqlUpdate;
 
-//cria instrução de UPDATE
-$sql = new TSqlUpdate;
-//define a entidade
-$sql->setEntity('aluno');
-// atribui o valor de cada coluna
-$sql->setRowData('nome','Pedro Cardoso da Silva');
-$sql->setRowData('rua','Machado de Assis');
-$sql->setRowData('fone','(88)5555');
+    //define a entidade 
+    $sql->setEntity('aluno');
 
-// define o critério de seleção de dados 
-$sql->setCriteria($criteria);
+    // atribui o valor de cada coluna
+    $sql->setRowData('nome','Pedro Cardoso da Silva');
+    $sql->setRowData('rua','Machado de Assis');
+    $sql->setRowData('fone','(88)5555');
 
-// processa instrução SQL
+    // define o critério de seleção de dados 
+    $sql->setCriteria($criteria);
 
-echo $sql->getInstruction();
+    // processa instrução SQL
+    echo $sql->getInstruction();
 
-echo "<br>\n";
+    echo "<br>\n";
 
 ?>
